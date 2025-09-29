a6c3a56278914b356d45fbd038f469b1e9395a00
Teris Cooper <admin@teris-cooper.de>
2025-09-29 22:16:49 +0200

    MODULVERLAGERUNG
    Alle Module zur besseren Organisation ausgelagert

--- src/module/proxmox/templates/parts/footer.php
+++
@@ -1,9 +0,0 @@
-<!-- Proxmox Module JavaScript - Modular Structure -->
-<script src="module/proxmox/templates/assets/proxmox-core.js"></script>
-<script src="module/proxmox/templates/assets/proxmox-node-selection.js"></script>
-<script src="module/proxmox/templates/assets/proxmox-server-list.js"></script>
-<script src="module/proxmox/templates/assets/proxmox-server-management.js"></script>
-<script src="module/proxmox/templates/assets/proxmox-vm-creation.js"></script>
-<script src="module/proxmox/templates/assets/proxmox-extended-features.js"></script>
-<script src="module/proxmox/templates/assets/proxmox-confirmation.js"></script>
-<script src="module/proxmox/templates/assets/proxmox-init.js"></script>
--- src/module/terminal/templates/footer.php
+++
@@ -1,6 +0,0 @@
-<h1>Footer Information</h1>
-
-<link rel="stylesheet" href="/src/module/terminal/assets/module.css">
-
-<!-- Include JavaScript -->
-<script src="/src/module/terminal/assets/module.js"></script>
